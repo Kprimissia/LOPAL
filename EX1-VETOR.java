5c68d73795c853a2109f14c0f64fa1c5b35ee6da
Kprimissia <159180445+Kprimissia@users.noreply.github.com>
2024-05-21 09:47:35 -0300

    Add files via upload

--- EX1-VETOR.java
+++ EX1-VETOR.java
@@ -5,20 +5,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        // Declara e inicializa o vetor com 8 elementos
         int[] numeros = new int[8];
 
-        // Solicita ao usuário que informe os 8 números
         System.out.println("Informe 8 números inteiros:");
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] = entrada.nextInt();
         }
 
-        // Variáveis para armazenar o maior valor e sua posição
         int maiorElemento = numeros[0];
         int posicaoMaior = 0;
 
-        // Percorre o vetor para encontrar o maior elemento e sua posição
         for (int i = 1; i < numeros.length; i++) {
             if (numeros[i] > maiorElemento) {
                 maiorElemento = numeros[i];
@@ -26,7 +22,6 @@ public class Main {
             }
         }
 
-        // Exibe o maior elemento e sua posição
         System.out.println("O maior elemento é: " + maiorElemento);
         System.out.println("Sua posição no vetor é: " + posicaoMaior);
     }
