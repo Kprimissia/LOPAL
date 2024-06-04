8df3b9bb280d86291078a575aabe61c1d8c7932d
Kprimissia <159180445+Kprimissia@users.noreply.github.com>
2024-06-04 10:32:27 -0300

    Add files via upload

---
+++ Java/Vetor/Exercício 1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int[] numeros = new int[8];
+
+        System.out.println("Informe 8 números inteiros:");
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = entrada.nextInt();
+        }
+
+        int maiorElemento = numeros[0];
+        int posicaoMaior = 0;
+
+        for (int i = 1; i < numeros.length; i++) {
+            if (numeros[i] > maiorElemento) {
+                maiorElemento = numeros[i];
+                posicaoMaior = i;
+            }
+        }
+
+        System.out.println("O maior elemento é: " + maiorElemento);
+        System.out.println("Sua posição no vetor é: " + posicaoMaior);
+    }
+}
---
+++ Java/Vetor/Exercício 2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[10];
+        int soma = 0;
+
+        System.out.println("Insira dez números inteiros:");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Número " + (i + 0) + ": ");
+            numeros[i] = scanner.nextInt();
+            soma += numeros[i]; 
+        }
+        scanner.close();
+
+        System.out.println("A soma dos 10 números inseridos é: " + soma);
+    }
+}
